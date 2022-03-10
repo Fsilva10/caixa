340d869c40982d3c429b000102784c78ea1a1d87
Filipe Silva <210100344@esg.ipsantarem.pt>
2022-03-10 17:46:38 +0000

    Metodos

--- src/caixa/Application.java
+++ src/caixa/Application.java
@@ -5,25 +5,30 @@ public class Application {
 	public static void main(String[] args) {
 		
 		// TOPO
-for (int i=0; i < 9; i++) {
-	System.out.print("0");
-}
-	System.out.println("");
+		horizontal();
 		
 		// LATERAL
-for (int i=0; i < 9; i++) {
-
-	System.out.print("0");
-	System.out.print("       ");
-	System.out.print("0");
-	System.out.println("");
-}
+		vertical();
 		
-		// FIM
-
-for (int i=0; i < 9; i++) {
-	System.out.print("0");
+		//FIM
+		horizontal();
+		
 	}
-
+		private static void horizontal() {
+	for (int i=0; i < 9; i++) {
+		System.out.print("0");
+	}
+		System.out.println("");
 }
+
+	
+	private static void vertical() {
+		for (int i=0; i < 9; i++) {
+
+			System.out.print("0");
+			System.out.print("       ");
+			System.out.print("0");
+			System.out.println("");
+		}
+		}
 }
